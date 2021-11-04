1f4234541b92abd437e2ce215f7ef5238daeab2e
MartenAlan <martenalan97@yahoo.de>
2021-11-04 12:02:30 +0100

    mit User und Calendar weitergemacht. Bitte kommunikation.docx lesen.

--- Code/User.py
+++ Code/User.py
@@ -1,13 +1,10 @@
-import mysql.connector
-
 class User:
     def __init__(self, name, email, isattendee):
         self.name = name
         self.email = email
         self.isattendee = isattendee
 
-    def insertIntoDB(self):
-        insertUser = f"INSERT INTO `user`(`name`, `email`, `isattendee`) VALUES ({name},{email},{isattendee})"
-        mycursor = mydb.cursor()
-        mycursor.execute(insertUser)
-        mycursor.commit()
+    def insertUser(self):
+        sql_insertUser = f"INSERT INTO `user`(`name`, `email`, `isattendee`)" \
+                     f" VALUES ({self.name},{self.email},{self.isattendee}) "
+        return sql_insertUser
--- Code/VCalendar.py
+++ Code/VCalendar.py
@@ -1,2 +1,17 @@
 class VCalendar:
-    pass
+    def __init__(self, userID, name, description, prodid, version, calscale, method, xprop, ianaprop):
+        self.userID = userID
+        self.name = name
+        self.description = description
+        self.prodid = prodid
+        self.version = version
+        self.calscale = calscale
+        self.method = method
+        self.xprop = xprop
+        self.ianaprop = ianaprop
+
+    def InsertCalendar(self):
+        sql_insertCalendar = f"INSERT INTO `vcalendar`(`userID`, `name`, `description`, `prodid`, `version`, `calscale`, `method`, `xprop`, `ianaprop`)" \
+                        f"VALUES ({self.userID},{self.name},{self.description},{self.prodid}," \
+                         f"{self.version},{self.calscale},{self.method},{self.xprop}, {self.ianaprop})"
+        return sql_insertCalendar
