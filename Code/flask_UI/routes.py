2e8d82c8ee9fd7f91a286ca6f06af309e92caf56
MartenAlan <martenalan97@yahoo.de>
2021-11-19 11:28:56 +0100

    Kalender kann von User erstellt werden. Radio Button funktionieren.

--- Code/flask_UI/routes.py
+++ Code/flask_UI/routes.py
@@ -3,34 +3,25 @@ from flask_UI import app, db
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 from coop.User import User
+from coop.VCalendar import VCalendar
 
 
 @app.route('/pythonlogin/', methods=['GET', 'POST'])
 def login():
-    # Output message if something goes wrong...
     msg = ''
-    # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'name' in request.form and 'email' in request.form:
-        # Create variables for easy access
         name = request.form['name']
         email = request.form['email']
-        # Check if account exists using MySQL
         cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM user WHERE name = %s AND email = %s', (name, email,))
-        # Fetch one record and return result
         account = cursor.fetchone()
-        # If account exists in accounts table in out database
         if account:
-            # Create session data, we can access this data in other routes
             session['loggedin'] = True
             session['ID'] = account['ID']
             session['name'] = account['name']
-            # Redirect to home page
             return redirect(url_for('home'))
         else:
-            # Account doesnt exist or username/password incorrect
             msg = 'Incorrect username/password!'
-    # Show the login form with message (if any)
     return render_template('home.html', msg=msg)
 
 
@@ -45,31 +36,55 @@ def about():
 @app.route("/home")
 def home():
     if 'loggedin' in session:
-        return render_template("home.html", loggedin = True, msg="")
-    return render_template("home.html", msg="", loggedin = False)
+        return render_template("home.html", loggedin=True, msg="")
+    return render_template("home.html", msg="", loggedin=False)
 
 
 @app.route("/createEvent")
 def createEvent():
-    return render_template("createEvent.html", title='Account')
+    if 'loggedin' in session:
+        return render_template("createEvent.html", title='Account', loggedin=True)
+    else:
+        redirect(url_for('home'))
 
 
-@app.route("/createCalender")
-def createCalender():
-    return render_template("createCalender.html", title='create Calender')
+@app.route("/createCalendar", methods=['GET', 'POST'])
+def createCalendar():
+    if 'loggedin' in session:
+        if request.method == 'POST' and 'name_C' in request.form:
+            # Create variables for easy access
+            name = request.form['name_C']
+            try:
+                beschreibung = request.form['beschreibung']
+            except:
+                beschreibung = ""
+            cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('SELECT * FROM vcalendar WHERE name = %s', (name,))
+            calender = cursor.fetchone()
+            if calender:
+                msg = 'Calendar already exists!'
+            else:
+                vcalendar = VCalendar(session["ID"], name, beschreibung)
+                cursor.execute(vcalendar.insertCalendar())
+                msg="Kalender wurde erstellt."
+                db.connection.commit()
+            return render_template("createCalendar.html", title='create Calendar', loggedin=True, msg=msg)
+        else:
+            return render_template("createCalendar.html", title='create Calendar', loggedin=True)
+    else:
+        redirect(url_for('home'))
 
 
 @app.route('/pythonlogin/logout')
 def logout():
     # Remove session data, this will log the user out
-   session.pop('loggedin', None)
-   session.pop('id', None)
-   session.pop('username', None)
-   # Redirect to login page
-   return redirect(url_for('login'))
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    # Redirect to login page
+    return redirect(url_for('login'))
 
 
-# http://localhost:5000/pythinlogin/register - this will be the registration page, we need to use both GET and POST requests
 @app.route('/pythonlogin/register', methods=['GET', 'POST'])
 def register():
     # Output message if something goes wrong...
@@ -88,7 +103,12 @@ def register():
             msg = 'Account already exists!'
         else:
             # Account doesnt exists and the form data is valid, now insert new account into accounts table
-            cursor.execute(f"INSERT INTO `user`(`name`, `email`, `isattendee`) VALUES ('{name}','{email}','');")
+            user = User(name, email)
+            cursor.execute(user.insertUser())
+            db.connection.commit()
+            calendar = VCalendar('(SELECT ID FROM user ORDER BY ID DESC LIMIT 1)', "Hauptkalendar",
+                                 name + "s Startkalender")
+            cursor.execute(calendar.insertCalendar())
             db.connection.commit()
             msg = 'You have successfully registered!'
     elif request.method == 'POST':
